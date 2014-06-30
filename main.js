fc6df38dd2870c96d221324c91a80c6c9a0d456c
Cory Robinson <coryrobinson42@gmail.com>
2014-06-30 11:56:46 -0600

    add js mouse scroll/move map

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-$(document).on('ready', function() {
-  
+$(document).mousemove(function(e) {
+    var percentY = e.clientY / $(window).height();
+    var percentX = e.clientX / $(window).width();
+    $('body, html').scrollTop($(document).height() * percentY);
+    $('body, html').scrollLeft($(document).width() * percentX);
 });
