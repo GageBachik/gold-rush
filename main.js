62f243824fa7679548b480231f75a0091806317b
gbachik <gbachik@gmail.com>
2014-06-30 12:04:58 -0600

    Finished markers and scrollling

--- main.js
+++ main.js
@@ -3,4 +3,19 @@ $(document).mousemove(function(e) {
     var percentX = e.clientX / $(window).width();
     $('body, html').scrollTop($(document).height() * percentY);
     $('body, html').scrollLeft($(document).width() * percentX);
+
+
+	$(document).on('click', function(event){
+		var marker = '<img class="marker" src="http://iprovweb.com/wp-content/uploads/2012/09/blue-pin.png"></img>'
+		var css = {
+			position : 'absolute',
+			left : event.pageX,
+			top : event.pageY,
+			height : '24px',
+			width : '24px',
+			zIndex : '999'
+		};
+		$('body').append($(marker).css(css));
+	});
+
 });
